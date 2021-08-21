7575bd7a7ae6af9c5c1dfc3bc70703a39e119541
muzammil852 <m.m.muzammilraja@gmail.com>
2021-08-20 23:35:55 -0700

    I Change Styleing In App

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
-let list = document.getElementById('list')
+let lists = document.getElementById("list");
 function Add__Todo__list() {
-    let todolist = document.getElementById('todo__id')
-    if (todo__id.value === "") {
-        alert("Please fill the task field")
-    }
-    else{
-        // Creat Li Tag Items And TextNode
-        let litext = document.createTextNode(todo__id.value)
-        let li = document.createElement('li')
-        li.appendChild(litext)
-        list.appendChild(li)
-        todo__id.value = ""
+  let todolist = document.getElementById("todo__id");
+  if (todo__id.value === "") {
+    alert("Please fill the task field");
+  } else {
+    // Creat Li Tag Items And TextNode
+    litext = document.createTextNode(todo__id.value);
+    pElement = document.createElement("p");
+    pElement.appendChild(litext);
+    li = document.createElement("li");
+    li.appendChild(pElement);
+    lists.appendChild(li);
+    todo__id.value = "";
 
+    // create Div
+    var divs = document.createElement("div");
 
-        // create Div
-        var divs = document.createElement('div')
-        li.appendChild(divs)
+    // Creat Edit Button
+    editbtn = document.createElement("button");
+    edittext = document.createTextNode("Edit");
+    editbtn.setAttribute("class", "Edit");
+    editbtn.setAttribute("onclick", "Edit_Items(this.parentNode)");
+    editbtn.appendChild(edittext);
+    // li.appendChild(editbtn)
+    divs.appendChild(editbtn);
 
-        // Creat Edit Button
-        let editbtn = document.createElement('button')
-        let edittext = document.createTextNode('Edit')
-        editbtn.setAttribute("class", "Edit")
-        editbtn.setAttribute("onclick", "Edit_Items(this)")
-        editbtn.appendChild(edittext)
-        li.appendChild(editbtn)
-        divs.appendChild(editbtn)
-
-        // Creat Delete Button
-        let deltbtn = document.createElement('button')
-        let delttext = document.createTextNode('delete')
-        deltbtn.setAttribute("class", "dltbtn")
-        deltbtn.setAttribute("onclick", "deleteitem(this)")
-        deltbtn.appendChild(delttext)
-        li.appendChild(deltbtn)
-        divs.appendChild(deltbtn)
-    }
+    // Creat Delete Button
+    let deltbtn = document.createElement("button");
+    let delttext = document.createTextNode("delete");
+    deltbtn.setAttribute("class", "dltbtn");
+    deltbtn.setAttribute("onclick", "deleteitem(this.parentNode)");
+    deltbtn.appendChild(delttext);
+    // li.appendChild(deltbtn)
+    divs.appendChild(deltbtn);
+    li.appendChild(divs);
+  }
 }
 
 // Delete Button Creat Function
 function deleteitem(e) {
-    e.parentNode.remove()
+  e.parentNode.remove();
 }
 
 // Delete All Button Creat Function
 function Delete__All() {
-    list.innerHTML = ""
+  lists.innerHTML = "";
 }
 // Edit Button Creat Function
 function Edit_Items(e) {
-    var value = prompt("Enter Updated Value", e.parentNode.firstChild.nodeValue)
-    e.parentNode.firstChild.nodeValue = value
-
+  let textForEdit = e.previousSibling;
+  if (textForEdit.style.border !== "1px solid green"){
+    textForEdit.setAttribute("contenteditable", "true");
+    textForEdit.style.border = "1px solid green";
+  }
+  else {
+    textForEdit.removeAttribute("contenteditable");
+    textForEdit.style.border = "none";
+  }
 }
